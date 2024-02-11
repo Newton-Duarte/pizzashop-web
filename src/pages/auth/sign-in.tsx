1cf3c1059d5c1cbf7afff12fe34e406371d81da3
Newton Duarte <newton_@hotmail.com>
2024-02-11 11:48:41 -0300

    feat: add sonner toasts

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,6 +1,7 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import * as zod from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -25,6 +26,7 @@ export function SignIn() {
   async function handleSingIn(data: SignFormData) {
     console.log(data)
     await new Promise((resolve) => setTimeout(resolve, 2000))
+    toast.success('Enviamos um link de autenticação para seu e-mail')
   }
 
   return (
